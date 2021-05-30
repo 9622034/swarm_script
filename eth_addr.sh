aea5785ef7e0c8bd3dcd72b6bf7f6043a1d82dee
9622034 <31524603+9622034@users.noreply.github.com>
2021-05-30 22:10:43 +0800

    Update eth_addr.sh

--- eth_addr.sh
+++ eth_addr.sh
@@ -1,8 +1,1 @@
-i=$1
-let group=$2
-echo $group
-while [ "$i" -le "$group" ]; do
-ls /bee_clef/clef-"$i"/keystore/ |awk -F  "--" '{print"0x" $3}'        
-echo $i
-        let i++
-done
+ls /bee_clef/clef-3/keystore/ |awk -F  "--" '{print"0x" $3}'        
