bdcb416b6bc22e970c0f82924199eb7c8a8fdc79
9622034 <31524603+9622034@users.noreply.github.com>
2021-05-31 11:32:13 +0800

    Update create_docker.sh

--- create_docker.sh
+++ create_docker.sh
@@ -1,9 +1,5 @@
-mkdir /bee_node
-chmod 777 /bee_node
-mkdir /bee_node/bee-3
-chmod 777 /bee_node/bee-3
-mkdir /bee_clef
-chmod 777 /bee_clef
-mkdir /bee_clef/clef-3
-chmod 777 /bee_clef/clef-3
+ mkdir /bee_node/bee-3
+ chmod 777 /bee_node/bee-3
+ mkdir /bee_clef/clef-3
+ chmod 777 /bee_clef/clef-3
 docker-compose  -f ./node_3/docker-compose_3.yml up -d
